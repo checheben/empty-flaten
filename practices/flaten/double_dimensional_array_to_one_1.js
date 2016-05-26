0044bfb57aeee6f1f6bb37f674fa15e047bbcf70
checheben <89188446@qq.com>
2016-05-26 10:29:41 +0800

    add two files

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,18 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+	var arr = [];
+	for(var i=0; i<collection.length; i++){
+		if(typeof collection[i] == "Array"){
+			for(var j=0; j<collection[j].length; j++){
+				arr.push(collection[i][j]);
+			}
+		}else{
+			arr.push(collection[i]);
+		}
+	}
+	
+	return arr;
 }
 
 module.exports = double_to_one;
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,24 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+	var arr = [];
+	var returnArr = [];
+	for(var i=0; i<collection.length; i++){
+		if(typeof collection[i] == "Array"){
+			for(var j=0; j<collection[j].length; j++){
+				arr.push(collection[i][j]);
+			}
+		}else{
+			arr.push(collection[i]);
+		}
+	}
+	arr = arr.toString().split(',');
+	for(i=0; i<arr.length; i++){
+		if(returnArr.indexOf(arr[i]) == -1){
+			returnArr.push(arr[i]);
+		}
+	}
+	return returnArr;
 }
 
 module.exports = double_to_one;
